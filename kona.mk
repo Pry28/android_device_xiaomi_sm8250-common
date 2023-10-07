1a5205bf7666211c06a71d1f0779f189e098ee86
madmax7896 <madmax78968@gmail.com>
2023-10-07 23:24:26 +0530

    sm8250-common: Import Dolby audio blobs

--- kona.mk
+++ kona.mk
@@ -250,6 +250,10 @@ PRODUCT_PACKAGES += \
 
 $(call soong_config_set,stagefright,target_disable_thumbnail_block_model,true)
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml
+
+
 # NFC
 PRODUCT_PACKAGES += \
     android.hardware.nfc-service.nxp \
