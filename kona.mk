24090d4e912ec2b9035137a0fb48833d0d09c2aa
Olzhas-Kdyr <kdiroljas@gmail.com>
2025-10-30 07:28:12 +0500

    Revert "sm8250-common: Switch to NCM USB tethering"
    * "The connection to the computer was established without internet access". Let's revert this commit to fix the issue.
    
    This reverts commit ae73a184ac80c71840e0762b920cce29fe4d0cf7.

--- kona.mk
+++ kona.mk
@@ -447,7 +447,6 @@ PRODUCT_PACKAGES += \
     hostapd \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
-    NcmTetheringOverlay \
     WifiResCommon \
     wpa_supplicant \
     wpa_supplicant.conf
