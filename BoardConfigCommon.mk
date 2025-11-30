8dcd9f20883d29d66885d67ffa7053b4f4883b20
Pry28 <apohuj789@gmail.com>
2025-11-30 19:47:14 +0500

    sm8250-common: Add back FCM

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -219,6 +219,7 @@ endif
 
 # VINTF
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(COMMON_PATH)/configs/hidl/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
